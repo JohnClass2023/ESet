7de6a58c74a36b5d44467d91f0ac68f2162008c6
Tianyu Wang <wty500@sjtu.edu.cn>
2024-05-21 21:33:03 +0800

    sync a feature with speedtest

--- test/2.cpp
+++ test/2.cpp
@@ -12,7 +12,7 @@ int main() {
 //    start = clock();
     ESet<long long> s[25];
     ESet<long long>::iterator it;
-    int op, lst=0, valid = 0,cnt=1;
+    int op, lst=0, it_a=-1, valid = 0, cnt=1;
     while (scanf("%d", &op) != EOF) {
         long long a, b, c;
 //        if(cnt==213){
@@ -23,6 +23,7 @@ int main() {
                 scanf("%lld%lld", &a, &b);
                 auto p=s[a].emplace(b);
                 if(p.second) {
+                	it_a = a;
                     it = p.first;
                     valid = 1;
                 }
@@ -30,7 +31,7 @@ int main() {
             }
             case 1:
                 scanf("%lld%lld", &a, &b);
-                if (valid && *it == b)valid = 0;
+                if (valid && it_a==a && *it == b)valid = 0;
                 s[a].erase(b);
                 break;
             case 2:
@@ -42,6 +43,7 @@ int main() {
                 auto it2 = s[a].find(b);
                 if (it2 != s[a].end()) {
                     printf("true\n");
+                    it_a = a;
                     it = it2;
                     valid = 1;
                 } else
